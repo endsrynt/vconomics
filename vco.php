cd0e86d16be7471ad5aac2e2a1669ffb4ed214c9
endsrynt <84342810+endsrynt@users.noreply.github.com>
2022-01-05 15:50:29 +0700

    Update vco.php

--- vco.php
+++ vco.php
@@ -3,8 +3,8 @@ error_reporting(0);
 echo "\e[0;33m[!] Reff: \e[0m";
 $reff =trim(fgets(STDIN));
 
+start:
 //RANDOM NAME
-login:
 function nama()
 	{
 	$ch = curl_init();
@@ -31,30 +31,31 @@ function nama()
       return $str;
   }
 
-    function angkarand($panjang)
-    {
-        $karakter= '1234567890';
-        $string = '';
-        for ($i = 0; $i < $panjang; $i++) {
-      $pos = rand(0, strlen($karakter)-1);
-      $string .= $karakter{$pos};
-        }
-        return $string;
-    }    
+  function angkarand($length)
+  {
+      $str        = "";
+      $characters = '1234567890';
+      $max        = strlen($characters) - 1;
+      for ($i = 0; $i < $length; $i++) {
+          $rand = mt_rand(0, $max);
+          $str .= $characters[$rand];
+      }
+      return $str;
+  } 
 
 $nama = explode(" ", nama());
 $nama1 = strtolower($nama[0]);
 $nama2 = strtolower($nama[1]);
 $rand = angkarand(5);
 $namalengkap = "$nama1$nama2$rand";
-$domain = "smilevxer.com";
+$domain = "fheiesit.com";
 $email = "$namalengkap@$domain";
 //$reff = "NS5L1WRG";
 
 //CREATE EMAIL
 $curl = curl_init();
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://generator.email/check_adres_validation3.php',
+  CURLOPT_URL => 'https://emailfake.com/check_adres_validation3.php',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
@@ -78,10 +79,29 @@ if($httpcode1 == 400){
      echo  "\e\33[32;1mBerhasil membuat email $email\n";
  }
 
+    //GET IP
+    $curl = curl_init();
+    
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => 'https://v4.myipstack.com/',
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_ENCODING => '',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 0,
+      CURLOPT_FOLLOWLOCATION => true,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+    
+    $ip = curl_exec($curl);
+    
+    curl_close($curl);
+    echo "\e[0;33m[!]\e[0m IP      : \e[0;32m$ip\e[0m\n";
+ 
 //REGISTER
 $curl = curl_init();
 curl_setopt_array($curl, array(
-  CURLOPT_URL => 'https://vconomics.io/identity/accounts/register/4',
+  CURLOPT_URL => 'https://id.vscore.vn/api-v1/accounts/register/4',
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => '',
   CURLOPT_MAXREDIRS => 10,
@@ -91,21 +111,23 @@ curl_setopt_array($curl, array(
   CURLOPT_CUSTOMREQUEST => 'POST',
   CURLOPT_POSTFIELDS =>'{"userName":"'.$email.'","password":"Yesbisa123","rePassword":"Yesbisa123","fromReferralId":"'.$reff.'","fullName":"'.$nama1.' '.$nama2.'"}',
   CURLOPT_HTTPHEADER => array(
-            'Accept: application/json, text/plain, */*',
-            'Accept-Encoding: gzip, deflate, br',
-            'Accept-Language: en-US,en;q=0.9,id;q=0.8,zh;q=0.7,ko;q=0.6,ja;q=0.5,zh-CN;q=0.4',
-            'Content-Type: application/json',
-            'Host: vconomics.io',
-            'Origin: https://vconomics.io',
-            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-            'X-CULTURE-CODE: EN',
-            'X-VIA: 2',
+              'Host: id.vscore.vn',
+              'accept: application/json, text/plain, */*',
+              'x-ip-address: '.$ip,
+              'x-user-agent: Mozilla/5.0 (Linux; Android 7.1.2; SM-G935FD Build/N2G48H; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/68.0.3440.70 Mobile Safari/537.36',
+              'x-location: ',
+              'x-via: 3',
+              'x-culture-code: EN',
+              'content-type: application/json;charset=utf-8',
+              'accept-encoding: gzip',
+              'user-agent: okhttp/3.12.1'
   ),
 ));
 
 $response = curl_exec($curl);
 $httpcode2 = curl_getinfo($curl, CURLINFO_HTTP_CODE);
 curl_close($curl);
+echo $response;
 if ($httpcode2 == 200) {
   $result1 = json_decode($response);
   if ($result1->success=="true"){
@@ -114,17 +136,17 @@ if ($httpcode2 == 200) {
     echo  "\e\33[32;1mBerhasil membuat akun $email\n";
     echo "\e[0;33m[!]\e[0m STATUS  : \e[0;32m$pesan\e[0m\n";
     echo "\e[0;33m[!]\e[0m TOKEN   : \e[0;32m$token\e[0m\n";
-    sleep(3);
 
 
     echo "\e[0;33m[!]\e[0m OTP     : ";
-    
-    for ($i=0; $i < 15; $i++) { 
+
+
+    for ($i=0; $i < 10; $i++) { 
 
       //CEK EMAIL
       $curl = curl_init();
       curl_setopt_array($curl, array(
-      CURLOPT_URL => 'https://generator.email/'.$domain.'/'.$namalengkap,
+      CURLOPT_URL => 'https://emailfake.com/',
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_ENCODING => '',
       CURLOPT_MAXREDIRS => 10,
@@ -140,21 +162,21 @@ if ($httpcode2 == 200) {
       curl_close($curl);
       $otp=explode('</p>',explode('<p style="color: #fa7800; font-weight: bold; text-align: center; font-size: 40px">',$response)[1])[0];
     
-      if ($otp==null){
-        continue;
-      }else{
+
+      if ($otp){
         echo "\e[0;32m$otp\e[0m\n";
-        break;
         goto input;
+        break;
+      }else{
+        continue;
       }
     };
-	  
 
-	input:
+    input:
     //INPUT OTP
     $curl = curl_init();
     curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://vconomics.io/identity/tokens/verify-otp',
+    CURLOPT_URL => 'https://id.vscore.vn/api-v1/tokens/verify-otp',
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_ENCODING => '',
     CURLOPT_MAXREDIRS => 10,
@@ -164,15 +186,11 @@ if ($httpcode2 == 200) {
     CURLOPT_CUSTOMREQUEST => 'POST',
     CURLOPT_POSTFIELDS =>'{"validateToken":"'.$token.'","otp":"'.$otp.'","otpType":1}',
     CURLOPT_HTTPHEADER => array(
-            'Accept application/json, text/plain, */*',
-            'Accept-Encoding: gzip, deflate, br',
-            'Accept-Language: en-US,en;q=0.9,id;q=0.8,zh;q=0.7,ko;q=0.6,ja;q=0.5,zh-CN;q=0.4',
+            'User-Agent: okhttp/3.12.1',
+            'x-culture-code: EN',
+            'x-location: ',
             'Content-Type: application/json',
-            'Host: vconomics.io',
-            'Origin: https://vconomics.io',
-            'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
-            'X-CULTURE-CODE: EN',
-            'X-VIA: 2',
+            
       ),
     ));
     $response = curl_exec($curl);
@@ -181,16 +199,20 @@ if ($httpcode2 == 200) {
     if ($result->success=="true"){
       $pesan2 = $result->messageCode;
       echo "\e[0;33m[!]\e[0m STATUS  : \e[0;32m$pesan2\e[0m\n";
-      goto login;
+      $file = fopen("reff_vco.txt","a");  
+      fwrite($file,"".$email); 
+      fwrite($file,"\n"); 
+      fclose($file);  
+      goto start;
     }else{
-      echo  "\e\33[31;1mGAGAL MEMASUKKAN OTP\n";
-      goto login;
+      echo  "\e\33[31;1mGagal Mendapatkan OTP\n";
+      goto start;
     }
   }else{
     echo  "\e\33[31;1mGagal Melakukan registrasi\n";
-    goto login;
+    goto start;
   }
 } else {
   echo  "\e\33[31;1mGagal Melakukan registrasi\n";
-  goto login;
+  goto start;
 }
